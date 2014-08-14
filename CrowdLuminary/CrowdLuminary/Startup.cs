40f025b4d7e93b13fdf99ad14f4af94632f7f70f
clattner <chris.lattner@live.com>
2014-08-14 10:02:04 -0500

    First_Commit

---
+++ CrowdLuminary/CrowdLuminary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrowdLuminary.Startup))]
+namespace CrowdLuminary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
